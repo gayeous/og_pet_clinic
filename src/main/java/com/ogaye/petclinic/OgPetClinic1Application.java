a250ac34960ff343f42ab1adb492d44e37a375e2
ousmane.gaye <ousmane.gaye@INF17401.CSS.LAN>
2018-11-21 16:29:59 +0000

    initial spring boot

---
+++ src/main/java/com/ogaye/petclinic/OgPetClinic1Application.java
@@ -0,0 +1,12 @@
+package com.ogaye.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgPetClinic1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgPetClinic1Application.class, args);
+	}
+}
